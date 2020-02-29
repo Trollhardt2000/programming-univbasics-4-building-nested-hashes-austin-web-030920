4b5a57b8610eaaa832bdfeb169d582f27c79caf3
Jeff Fisk <jeff.fisk512@gmail.com>
2020-02-29 22:10:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
     	  railroads: {
     	    pieces: 4, 
     	    rent_in_dollars: {
-    	      one: "", two: "", thr: "", four: ""}, 
+    	      one_piece_owned: "25", two_pieces_owned: "", three_pieces_owned: "", four_pieces_owned: ""}, 
     	    names: {
     	      n1: "", n2:"", n3:"",n4:""}
     	  }
